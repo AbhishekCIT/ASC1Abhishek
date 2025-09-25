81f815acc69b2faf1741fac27f21822325e874b9
AbhishekCIT <20cs51@cit.edu.in>
2025-09-25 22:49:26 +0530

    Adding ScheduledReportingApplication.java via GitHubFileCreator

---
+++ 2025_09_25_17_16_44/code/src/main/java/com/example/scheduledreporting/ScheduledReportingApplication.java
@@ -0,0 +1,14 @@
+package com.example.scheduledreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the Scheduled Reporting Spring Boot application.
+ */
+@SpringBootApplication
+public class ScheduledReportingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledReportingApplication.class, args);
+    }
+}
