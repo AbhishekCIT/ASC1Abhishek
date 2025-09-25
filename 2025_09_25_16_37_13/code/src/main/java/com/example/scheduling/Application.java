ffc20368dd4f6d60183b4885260025158f99432b
AbhishekCIT <20cs51@cit.edu.in>
2025-09-25 22:10:32 +0530

    Adding Application.java via GitHubFileCreator

---
+++ 2025_09_25_16_37_13/code/src/main/java/com/example/scheduling/Application.java
@@ -0,0 +1,14 @@
+package com.example.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class for Automated Report Scheduling Feature.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
