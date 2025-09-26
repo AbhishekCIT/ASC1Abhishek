001d2c409fb61be35c5b9a7f6c95af007b6e1f86
AbhishekCIT <20cs51@cit.edu.in>
2025-09-26 20:25:26 +0530

    Adding BookingRequest.java via GitHubFileCreator

---
+++ 2025_09_26_14_51_59/code/src/main/java/com/example/airbooking/model/BookingRequest.java
@@ -0,0 +1,17 @@
+package com.example.airbooking.model;
+
+/**
+ * Model for booking request payload.
+ */
+public class BookingRequest {
+    private Long flightId;
+    private PassengerDetails passengerDetails;
+    private PaymentRequest paymentInfo;
+
+    public Long getFlightId() { return flightId; }
+    public void setFlightId(Long flightId) { this.flightId = flightId; }
+    public PassengerDetails getPassengerDetails() { return passengerDetails; }
+    public void setPassengerDetails(PassengerDetails passengerDetails) { this.passengerDetails = passengerDetails; }
+    public PaymentRequest getPaymentInfo() { return paymentInfo; }
+    public void setPaymentInfo(PaymentRequest paymentInfo) { this.paymentInfo = paymentInfo; }
+}
