a5090f2a160d4c2e9b3d9f708604cf248d2bfec2
AbhishekCIT <20cs51@cit.edu.in>
2025-09-26 23:40:13 +0530

    Adding UserRepository.java via GitHubFileCreator

---
+++ 2025_09_26_18_07_35/code/src/main/java/com/example/airbooking/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.airbooking.repository;
+
+import com.example.airbooking.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for User entity.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
