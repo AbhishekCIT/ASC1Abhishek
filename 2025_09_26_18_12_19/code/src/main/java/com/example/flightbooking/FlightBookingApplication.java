1520ffd908b7e913afcd866dfb51aa3b947662e4
AbhishekCIT <20cs51@cit.edu.in>
2025-09-26 23:46:00 +0530

    Adding FlightBookingApplication.java via GitHubFileCreator

---
+++ 2025_09_26_18_12_19/code/src/main/java/com/example/flightbooking/FlightBookingApplication.java
@@ -0,0 +1,14 @@
+package com.example.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class.
+ */
+@SpringBootApplication
+public class FlightBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlightBookingApplication.class, args);
+    }
+}
