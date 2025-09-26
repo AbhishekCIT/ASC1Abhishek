2d0de4fc77283a879bbedcac403b83b610831ae2
AbhishekCIT <20cs51@cit.edu.in>
2025-09-26 23:46:22 +0530

    Adding FlightBookingApplicationTest.java via GitHubFileCreator

---
+++ 2025_09_26_18_12_19/unit_tests/FlightBookingApplicationTest.java
@@ -0,0 +1,19 @@
+package com.example.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Test to ensure that the Spring application context loads successfully.
+ */
+@SpringBootTest
+public class FlightBookingApplicationTest {
+
+    /**
+     * Test that the application context loads without throwing exceptions.
+     */
+    @Test
+    void contextLoads() {
+        // The test will fail if the application context cannot start.
+    }
+}
