3e49a7366c11d5171d9612cad2dcbea3c4d6b88a
AbhishekCIT <20cs51@cit.edu.in>
2025-09-26 23:39:33 +0530

    Adding AirBookingApplication.java via GitHubFileCreator

---
+++ 2025_09_26_18_07_35/code/src/main/java/com/example/airbooking/AirBookingApplication.java
@@ -0,0 +1,14 @@
+package com.example.airbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class for Air Booking System.
+ */
+@SpringBootApplication
+public class AirBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirBookingApplication.class, args);
+    }
+}
