8b95467c782d2317d0c3bb2fbb73a8de55542efb
AbhishekCIT <20cs51@cit.edu.in>
2025-09-26 19:28:07 +0530

    Adding NotFoundException.java via GitHubFileCreator

---
+++ 2025_09_26_13_54_00/code/src/main/java/com/airtransport/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.airtransport.exception;
+
+/**
+ * Thrown when an entity is not found.
+ */
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message); }
+}
