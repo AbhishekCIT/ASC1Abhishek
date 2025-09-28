f14b9f31cb624fba0c58823e97211ecf0ad9c2e4
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 00:49:29 +0530

    Adding BoardingPassResponse.java via GitHubFileCreator

---
+++ 2025_09_28_19_15_01/code/src/main/java/com/example/airbooking/model/BoardingPassResponse.java
@@ -0,0 +1,11 @@
+package com.example.airbooking.model;
+
+/**
+ * Response model for boarding pass API.
+ */
+public class BoardingPassResponse {
+    private String boardingPass;
+
+    public String getBoardingPass() { return boardingPass; }
+    public void setBoardingPass(String boardingPass) { this.boardingPass = boardingPass; }
+}
