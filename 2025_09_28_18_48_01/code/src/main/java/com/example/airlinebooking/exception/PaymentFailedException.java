ea057b7a66e3e6a47ed9566cee4e5dc8e945fd66
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 00:21:06 +0530

    Adding PaymentFailedException.java via GitHubFileCreator

---
+++ 2025_09_28_18_48_01/code/src/main/java/com/example/airlinebooking/exception/PaymentFailedException.java
@@ -0,0 +1,10 @@
+package com.example.airlinebooking.exception;
+
+/**
+ * Exception thrown when payment fails.
+ */
+public class PaymentFailedException extends RuntimeException {
+    public PaymentFailedException(String message) {
+        super(message);
+    }
+}
