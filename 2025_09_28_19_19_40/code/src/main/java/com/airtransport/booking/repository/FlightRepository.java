122fa0357fd8aa4e38433ff3721a1a8cfa1facce
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 00:52:27 +0530

    Adding FlightRepository.java via GitHubFileCreator

---
+++ 2025_09_28_19_19_40/code/src/main/java/com/airtransport/booking/repository/FlightRepository.java
@@ -0,0 +1,12 @@
+package com.airtransport.booking.repository;
+
+import com.airtransport.booking.entity.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for Flight entity.
+ */
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+}
