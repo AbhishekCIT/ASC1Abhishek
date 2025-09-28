5fcc07a1077b5e5290085b5a256ef730262fd445
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 00:58:29 +0530

    Adding CalculatorApplication.java via GitHubFileCreator

---
+++ 2025_09_28_19_26_15/code/src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to bootstrap the Calculator Spring Boot application.
+ */
+@SpringBootApplication
+public class CalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+}
