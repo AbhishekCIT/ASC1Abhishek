c4c65d395a554ddc637bdec64cfee416c7fbca1c
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 17:19:48 +0530

    Adding FlightSearchApplication.java via GitHubFileCreator

---
+++ 2025_09_29_11_46_02/code/src/main/java/com/example/flightsearch/FlightSearchApplication.java
@@ -0,0 +1,14 @@
+package com.example.flightsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to bootstrap the Spring Boot application.
+ */
+@SpringBootApplication
+public class FlightSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlightSearchApplication.class, args);
+    }
+}
