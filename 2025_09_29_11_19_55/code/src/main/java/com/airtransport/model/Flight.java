643f8177423ba421bdd41b4385c717ced4ae4ac4
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 16:53:02 +0530

    Adding Flight.java via GitHubFileCreator

---
+++ 2025_09_29_11_19_55/code/src/main/java/com/airtransport/model/Flight.java
@@ -0,0 +1,33 @@
+package com.airtransport.model;
+
+/**
+ * Model representing a flight.
+ */
+public class Flight {
+    private String flightId;
+    private String airline;
+    private String origin;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+    private double price;
+    private int seatsAvailable;
+
+    // Getters and setters
+    public String getFlightId() { return flightId; }
+    public void setFlightId(String flightId) { this.flightId = flightId; }
+    public String getAirline() { return airline; }
+    public void setAirline(String airline) { this.airline = airline; }
+    public String getOrigin() { return origin; }
+    public void setOrigin(String origin) { this.origin = origin; }
+    public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
+    public String getDepartureTime() { return departureTime; }
+    public void setDepartureTime(String departureTime) { this.departureTime = departureTime; }
+    public String getArrivalTime() { return arrivalTime; }
+    public void setArrivalTime(String arrivalTime) { this.arrivalTime = arrivalTime; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public int getSeatsAvailable() { return seatsAvailable; }
+    public void setSeatsAvailable(int seatsAvailable) { this.seatsAvailable = seatsAvailable; }
+}
