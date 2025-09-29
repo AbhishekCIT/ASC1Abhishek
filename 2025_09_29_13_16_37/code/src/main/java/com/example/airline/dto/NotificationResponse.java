ccde16fe980773994cba574e9b7583a5a06cbebf
AbhishekCIT <20cs51@cit.edu.in>
2025-09-29 18:49:46 +0530

    Adding NotificationResponse.java via GitHubFileCreator

---
+++ 2025_09_29_13_16_37/code/src/main/java/com/example/airline/dto/NotificationResponse.java
@@ -0,0 +1,9 @@
+package com.example.airline.dto;
+
+public class NotificationResponse {
+    private String notificationStatus;
+
+    // Getters and Setters
+    public String getNotificationStatus() { return notificationStatus; }
+    public void setNotificationStatus(String notificationStatus) { this.notificationStatus = notificationStatus; }
+}
