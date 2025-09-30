71a812ab3d4cb30701f6047401bf715f4d2270df
AbhishekCIT <20cs51@cit.edu.in>
2025-09-30 21:07:11 +0530

    Adding PaymentRepository.java via GitHubFileCreator

---
+++ 2025_09_30_15_32_44/code/src/main/java/com/example/airlinebooking/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.example.airlinebooking.repository;
+
+import com.example.airlinebooking.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for Payment entity.
+ */
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+}
