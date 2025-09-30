ca06d245fc35909bf2edf30992a1f6b7e3c6f838
AbhishekCIT <20cs51@cit.edu.in>
2025-09-30 21:06:52 +0530

    Adding PaymentInfo.java via GitHubFileCreator

---
+++ 2025_09_30_15_32_44/code/src/main/java/com/example/airlinebooking/model/PaymentInfo.java
@@ -0,0 +1,21 @@
+package com.example.airlinebooking.model;
+
+/**
+ * Model for payment information (PCI DSS compliant fields only).
+ */
+public class PaymentInfo {
+    private String cardNumber;
+    private String cardHolder;
+    private String expiry;
+    private String cvv;
+
+    // Getters and setters
+    public String getCardNumber() { return cardNumber; }
+    public void setCardNumber(String cardNumber) { this.cardNumber = cardNumber; }
+    public String getCardHolder() { return cardHolder; }
+    public void setCardHolder(String cardHolder) { this.cardHolder = cardHolder; }
+    public String getExpiry() { return expiry; }
+    public void setExpiry(String expiry) { this.expiry = expiry; }
+    public String getCvv() { return cvv; }
+    public void setCvv(String cvv) { this.cvv = cvv; }
+}
