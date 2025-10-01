c5d85e9eda27816f505dee789d1990fd0de65f6b
AbhishekCIT <20cs51@cit.edu.in>
2025-10-01 13:14:36 +0530

    Adding User.java via GitHubFileCreator

---
+++ 2025_10_01_13_11_40/code/src/main/java/com/example/airlinebooking/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.airlinebooking.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Entity representing a user in the system.
+ */
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
