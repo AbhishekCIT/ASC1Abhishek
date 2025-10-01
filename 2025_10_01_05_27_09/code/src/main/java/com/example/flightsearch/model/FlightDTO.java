1721e6533763d5cfc6b76696f43a58b387968328
AbhishekCIT <20cs51@cit.edu.in>
2025-10-01 10:59:31 +0530

    Adding FlightDTO.java via GitHubFileCreator

---
+++ 2025_10_01_05_27_09/code/src/main/java/com/example/flightsearch/model/FlightDTO.java
@@ -0,0 +1,33 @@
+package com.example.flightsearch.model;
+
+/**
+ * Data Transfer Object for flight details in the search response.
+ */
+public class FlightDTO {
+    private String flightId;
+    private String airline;
+    private String origin;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+    private double price;
+    private String duration;
+
+    // Getters and setters
+    public String getFlightId() { return flightId; }
+    public void setFlightId(String flightId) { this.flightId = flightId; }
+    public String getAirline() { return airline; }
+    public void setAirline(String airline) { this.airline = airline; }
+    public String getOrigin() { return origin; }
+    public void setOrigin(String origin) { this.origin = origin; }
+    public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
+    public String getDepartureTime() { return departureTime; }
+    public void setDepartureTime(String departureTime) { this.departureTime = departureTime; }
+    public String getArrivalTime() { return arrivalTime; }
+    public void setArrivalTime(String arrivalTime) { this.arrivalTime = arrivalTime; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public String getDuration() { return duration; }
+    public void setDuration(String duration) { this.duration = duration; }
+}
