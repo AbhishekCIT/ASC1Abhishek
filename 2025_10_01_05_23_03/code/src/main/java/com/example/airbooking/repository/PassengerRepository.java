45eaecc8fc45860bf55dae627998e69d611faf9a
AbhishekCIT <20cs51@cit.edu.in>
2025-10-01 10:55:22 +0530

    Adding PassengerRepository.java via GitHubFileCreator

---
+++ 2025_10_01_05_23_03/code/src/main/java/com/example/airbooking/repository/PassengerRepository.java
@@ -0,0 +1,13 @@
+package com.example.airbooking.repository;
+
+import com.example.airbooking.entity.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for Passenger entity.
+ */
+@Repository
+public interface PassengerRepository extends JpaRepository<Passenger, Integer> {
+    Passenger findByEmail(String email);
+}
