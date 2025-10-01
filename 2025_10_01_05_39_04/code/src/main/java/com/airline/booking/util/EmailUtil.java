9bc40b815e7f4b92cfdc136da8a5d16d920bd681
AbhishekCIT <20cs51@cit.edu.in>
2025-10-01 11:13:17 +0530

    Adding EmailUtil.java via GitHubFileCreator

---
+++ 2025_10_01_05_39_04/code/src/main/java/com/airline/booking/util/EmailUtil.java
@@ -0,0 +1,43 @@
+package com.airline.booking.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+/**
+ * Utility class for sending emails.
+ */
+@Component
+@Slf4j
+public class EmailUtil {
+    private final JavaMailSender mailSender;
+
+    public EmailUtil(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    /**
+     * Send an email with the given parameters.
+     * @param to recipient email
+     * @param subject subject
+     * @param body body (HTML allowed)
+     */
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(message);
+            log.info("Email sent to {} with subject {}", to, subject);
+        } catch (MessagingException e) {
+            log.error("Failed to send email to {}: {}", to, e.getMessage());
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+}
