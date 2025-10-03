cb1fc426be0953cdf663079679cbd6042b5e5dd9
AbhishekCIT <20cs51@cit.edu.in>
2025-10-03 10:44:39 +0530

    Adding AirTransportApplication.java via GitHubFileCreator

---
+++ 2025_10_03_05_12_03/code/src/main/java/com/example/airtransport/AirTransportApplication.java
@@ -0,0 +1,14 @@
+package com.example.airtransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for Spring Boot application.
+ */
+@SpringBootApplication
+public class AirTransportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirTransportApplication.class, args);
+    }
+}
