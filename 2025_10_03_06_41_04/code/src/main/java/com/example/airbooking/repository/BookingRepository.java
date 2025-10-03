d21f03c5c2cb3e6403e1292489425aef321edc65
AbhishekCIT <20cs51@cit.edu.in>
2025-10-03 12:13:45 +0530

    Adding BookingRepository.java via GitHubFileCreator

---
+++ 2025_10_03_06_41_04/code/src/main/java/com/example/airbooking/repository/BookingRepository.java
@@ -0,0 +1,13 @@
+package com.example.airbooking.repository;
+
+import com.example.airbooking.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for Booking entity.
+ */
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Booking findByBookingRef(String bookingRef);
+}
