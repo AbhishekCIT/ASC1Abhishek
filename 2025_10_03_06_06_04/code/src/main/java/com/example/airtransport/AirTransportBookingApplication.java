196332b5a5736516d33b54218e50196aef420a72
AbhishekCIT <20cs51@cit.edu.in>
2025-10-03 11:38:28 +0530

    Adding AirTransportBookingApplication.java via GitHubFileCreator

---
+++ 2025_10_03_06_06_04/code/src/main/java/com/example/airtransport/AirTransportBookingApplication.java
@@ -0,0 +1,14 @@
+package com.example.airtransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class for Air Transport Booking System.
+ */
+@SpringBootApplication
+public class AirTransportBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirTransportBookingApplication.class, args);
+    }
+}
