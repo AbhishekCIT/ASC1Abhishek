790a07ef7176603da410b9dd061ae3bad9ebb462
AbhishekCIT <20cs51@cit.edu.in>
2025-10-03 11:14:41 +0530

    Adding PaymentInfo.java via GitHubFileCreator

---
+++ 2025_10_03_05_42_26/code/src/main/java/com/airtransport/model/PaymentInfo.java
@@ -0,0 +1,20 @@
+package com.airtransport.model;
+
+/**
+ * Model for payment information.
+ */
+public class PaymentInfo {
+    private String cardNumber;
+    private String cardHolderName;
+    private String expiryDate;
+    private String cvv;
+
+    public String getCardNumber() { return cardNumber; }
+    public void setCardNumber(String cardNumber) { this.cardNumber = cardNumber; }
+    public String getCardHolderName() { return cardHolderName; }
+    public void setCardHolderName(String cardHolderName) { this.cardHolderName = cardHolderName; }
+    public String getExpiryDate() { return expiryDate; }
+    public void setExpiryDate(String expiryDate) { this.expiryDate = expiryDate; }
+    public String getCvv() { return cvv; }
+    public void setCvv(String cvv) { this.cvv = cvv; }
+}
