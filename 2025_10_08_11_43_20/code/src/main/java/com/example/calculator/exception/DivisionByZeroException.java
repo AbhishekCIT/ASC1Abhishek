6851c0a4cc811c033510b56fef192b0ff76e8ce5
AbhishekCIT <20cs51@cit.edu.in>
2025-10-08 17:14:50 +0530

    Adding DivisionByZeroException.java via GitHubFileCreator

---
+++ 2025_10_08_11_43_20/code/src/main/java/com/example/calculator/exception/DivisionByZeroException.java
@@ -0,0 +1,10 @@
+package com.example.calculator.exception;
+
+/**
+ * Exception thrown when division by zero is attempted.
+ */
+public class DivisionByZeroException extends RuntimeException {
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
